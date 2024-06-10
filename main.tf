c328eb1f011e7bf4622e145f6a205bfc2fe86e32
brandanking72 <131664617+brandanking72@users.noreply.github.com>
2024-06-10 16:14:46 -0400

    adding new vpc, subnet, igw, rt

--- main.tf
+++ main.tf
@@ -22,32 +22,11 @@ provider "aws" {
 resource "aws_instance" "webserver" {
   ami                    = "ami-00beae93a2d981137"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-090ef33c74adf5f35"]
-  subnet_id              = "subnet-0d1fa6cfcff239187"
+  vpc_security_group_ids = "aws_security_group.vpc_id"
+  subnet_id              = "aws_instance.subnet_id"
 
   tags = {
     Name = "terraform-bk"
-  }
-}
-
-resource "aws_security_group" "websg" {
-  name        = "websg"
-  description = "Security group for web servers"
-
-  #// Ingress rule allowing inbound HTTP traffic from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  #// Egress rule allowing outbound traffic to anywhere
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -57,5 +36,77 @@ resource "aws_s3_bucket" "testbucket" {
   tags = {
     Name        = "terraform-bkbucket"
     Environment = "Dev"
+  }
+}
+
+# Create a VPC
+resource "aws_vpc" "web_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "web_vpc"
+  }
+}
+
+# Create a public subnet
+resource "aws_subnet" "web_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"  # Change the AZ as needed
+
+  tags = {
+    Name = "web_subnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "terraform-igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "route_table"
+  }
+}
+
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "route_table_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Create a Security Group
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web_sg"
   }
 }
