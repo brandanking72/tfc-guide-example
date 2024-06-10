4cb535003817732f3062cb3dc0c013e8f5e3c323
brandanking72 <131664617+brandanking72@users.noreply.github.com>
2024-06-10 12:56:15 -0400

    start from scratch, ec2

--- main.tf
+++ main.tf
@@ -2,46 +2,17 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
  }
 
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "terraform-ec2"
-
+resource "aws_instance" "app_server" {
+  ami                    = "ami-00beae93a2d981137"
   instance_type          = "t2.micro"
-  key_name               = "terraform-bk"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  #subnet_id              = "subnet-eddcdzz4"
+  vpc_security_group_ids = ["sg-090ef33c74adf5f35"]
+  subnet_id              = "subnet-0d1fa6cfcff239187"
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "terraform-bk"
   }
 }
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "terraform-bk-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
-}
-
-module "web_server_sg" {
-  source = "terraform-aws-modules/security-group/aws//modules/http-80"
-
-  name        = "web-server"
-  description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = "vpc-0d892bdb41900133c"
-
-  ingress_cidr_blocks = ["10.10.0.0/16"]
-}
